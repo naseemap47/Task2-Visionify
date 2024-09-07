085376ca77de182816d859d1df86df18401344f9
NASEEM A P <naseemalassampattil@gmail.com>
2024-09-07 11:05:16 +0530

    added Prepare sample video data

---
+++ prepare_data.sh
@@ -0,0 +1,10 @@
+echo "[INFO] Installing Gdown"
+pip install gdown==4.6.1
+mkdir -p videos
+cd videos
+gdown 1QSVS753KX0UQ2D1ts0U7xq04HgLramjw
+gdown 1RZidxjZ3OyAxrpKEReDIsbzKcrRJx8hL
+gdown 1IKlQhQLKYZmKUo-ip9U8xuZ9Y3Y6hJCS
+gdown 1msfSkete5EdmL5pWnBycaGbpzQpbiSHJ
+gdown 1nmHxA7YTUc-Fiv8YaUH7ids-ys-nMNdd
+echo "[COMPLETED] Sample video data downloaded."
